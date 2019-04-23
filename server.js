1960b3c333d27559b3e00b3a3d9377be4ea046f1
一木 <1208674494@qq.com>
2019-04-23 20:34:06 +0800

    。

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const fs = require('fs');
+const open = require('open');
+const child_process = require('child_process');
 const { router } = require('./lib/router');
 // const options = {
 //   key: fs.readFileSync('keys/agent2-key.pem'),
@@ -11,5 +13,14 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8080,function(){
-	console.log('http://localhost:8080/ 开启成功');
+	let serverUrl = 'http://localhost:8080/';
+	console.log(serverUrl,'开启成功');
+	child_process.exec('open ${serverUrl}',(error, stdout, stderr) => {
+	  if (error) {
+	    console.error(`执行出错: ${error}`);
+	    return;
+	  }
+	  console.log(`stdout: ${stdout}`);
+	  console.log(`stderr: ${stderr}`);
+	});
 });
